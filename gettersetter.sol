13ba047dd24dd13be7cfa5323fdb3ea45482fc50
abhishek-9036 <abhishek@magpiexyz.io>
2024-06-25 06:45:10 +0530

    Day 1 State Variable

---
+++ gettersetter.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.26;
+
+contract Check{
+
+    uint age = 10;
+
+    function getter() public view returns(uint)
+    {
+        return age;
+    } 
+
+    function setter(uint newage) public
+    {
+        age = newage;
+    }
+
+
+}
